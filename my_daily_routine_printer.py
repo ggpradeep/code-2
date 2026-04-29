e620ec539f3ba0dc56299c9c2a26fb33cbffb5e4
ggpradeep <80970765+ggpradeep@users.noreply.github.com>
2026-04-28 18:35:45 -0700

    Add files via upload

---
+++ my_daily_routine_printer.py
@@ -0,0 +1,9 @@
+
+print("I wake up at 6:45 AM")
+print("I brush at 7 AM ")
+print("I eat at 8 AM")
+print("I start school at 9 AM")
+print("I stay at school for:", 2*3, "hours")
+print("I play with my friends after school")
+print("I study for 1 hour and 30 minutes")
+print("I go to bed at 9:30 PM")
